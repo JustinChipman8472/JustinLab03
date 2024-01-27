45bd5d84e613553dcb7b3e28e643e58146df9509
JustinChipman8472 <n01598472@humber.ca>
2024-01-26 20:26:07 -0500

    init commit after deleting app modle

---
+++ app/src/main/java/justin/chipman/n01598472/MainActivity.java
@@ -0,0 +1,14 @@
+package justin.chipman.n01598472;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
